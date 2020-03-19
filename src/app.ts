d2c281061e4cbe119bccc73b67ba510f33187286
Guergeiro <business@brenosalles.com>
2020-03-19 15:06:50 +0000

    Updated bot to be more dynamic

--- src/app.ts
+++ src/app.ts
@@ -1,113 +1,85 @@
 if (process.env.NODE_ENV != "production") {
-  const dotenv = require(`dotenv`);
-  dotenv.config({
-    path: `${__dirname}/config.env`
-  });
+    const dotenv = require(`dotenv`);
+    dotenv.config({
+        path: `${__dirname}/config.env`
+    });
 }
 import fetch from "node-fetch";
 import { Client, Message } from "discord.js";
 import { Ementa } from "./ementas/Ementa";
 
 const sendEmentasToChannel = (ementas: Array<Ementa>, message: Message) => {
-  if (ementas.length == 0) {
-    message.channel.send(`No menus available!`);
-    return;
-  }
-  for (const ementa of ementas) {
-    message.channel.send(ementa.toString());
-  }
+    if (ementas.length == 0) {
+        message.channel.send(`No menus available!`);
+        return;
+    }
+    for (const ementa of ementas) {
+        message.channel.send(ementa.toString());
+    }
 };
 
-let ementas: Array<Ementa> = [];
+const processMessage = (date: Date, message: Message) => {
+    const filteredEmentas: Array<Ementa> = ementas.filter((ementa) => {
+        if (ementa.getDate().getUTCDate() != date.getUTCDate()) {
+            return;
+        }
+        if (ementa.getDate().getUTCMonth() != date.getUTCMonth()) {
+            return;
+        }
+        return ementa;
+    });
+    sendEmentasToChannel(filteredEmentas, message);
+};
+
+const ementas: Array<Ementa> = [];
 const client: Client = new Client();
 
 client.once("ready", async () => {
-  const response = await fetch("https://ementas-api.herokuapp.com/ementas");
+    const response = await fetch(`${process.env.API_URL}`);
 
-  (await response.json()).map((obj: any) => {
-    const ementa: Ementa = new Ementa(
-      new Date(obj["date"]),
-      obj["type"],
-      obj["sopa"],
-      obj["carne"],
-      obj["peixe"],
-      obj["dieta"],
-      obj["vegetariano"]
-    );
-    ementas.push(ementa);
-  });
+    (await response.json()).map((obj: any) => {
+        const ementa: Ementa = new Ementa(new Date(obj["date"]), obj["type"], obj["sopa"], obj["carne"], obj["peixe"], obj["dieta"], obj["vegetariano"]);
+        ementas.push(ementa);
+    });
 });
 
-client.on("message", message => {
-  if (message.author.bot) {
-    return;
-  }
-  if (message.content.startsWith("!") === false) {
-    return;
-  }
-  const args: Array<string> = message.content.slice("!".length).split(/ +/);
-  const commandName: string = (<string>args.shift()).toLowerCase();
+client.on("message", (message) => {
+    if (message.author.bot) {
+        return;
+    }
+    if (message.content.startsWith("!") === false) {
+        return;
+    }
+    const args: Array<string> = message.content.slice("!".length).split(/ +/);
+    const commandName: string = (<string>args.shift()).toLowerCase();
 
-  switch (commandName) {
-    case "help":
-      message.reply(
-        "the possible commands are:\n`!today` - sends today's menu.\n`!tomorrow` - sends tomorrow's menu.\n`!day dd-mm` - get a specific day menu."
-      );
-      break;
-    case "today":
-      const today: Date = new Date();
-      const todayEmentas: Array<Ementa> = ementas.filter(ementa => {
-        if (ementa.getDate().getUTCDate() != today.getUTCDate()) {
-          return;
-        }
-        if (ementa.getDate().getUTCMonth() != today.getUTCMonth()) {
-          return;
-        }
-        return ementa;
-      });
-      sendEmentasToChannel(todayEmentas, message);
-      break;
-    case "tomorrow":
-      const tomorrow: Date = new Date(
-        new Date().getUTCFullYear(),
-        new Date().getUTCMonth(),
-        new Date().getUTCDate() + 1
-      );
-      const tomorrowEmentas: Array<Ementa> = ementas.filter(ementa => {
-        if (ementa.getDate().getUTCDate() != tomorrow.getUTCDate()) {
-          return;
-        }
-        if (ementa.getDate().getUTCMonth() != tomorrow.getUTCMonth()) {
-          return;
-        }
-        return ementa;
-      });
-      sendEmentasToChannel(tomorrowEmentas, message);
-      break;
-    case "day":
-      if (args.length == 0) {
-        message.reply("wrong command. Please type `!help` to understand it.");
-        break;
-      }
-      const day: string = args[0].split("-")[0];
-      const month: string = args[0].split("-")[1];
-      const date: Date = new Date(
-        new Date().getUTCFullYear(),
-        +month - 1,
-        +day
-      );
-      const specificEmentas: Array<Ementa> = ementas.filter(ementa => {
-        if (ementa.getDate().getUTCDate() != date.getUTCDate()) {
-          return;
-        }
-        if (ementa.getDate().getUTCMonth() != date.getUTCMonth()) {
-          return;
-        }
-        return ementa;
-      });
-      sendEmentasToChannel(specificEmentas, message);
-      break;
-  }
+    switch (commandName) {
+        case "help":
+            message.reply(
+                "the possible commands are:\n`!today` - sends today's menu.\n`!tomorrow` - sends tomorrow's menu.\n`!day dd-mm` - get a specific day menu."
+            );
+            break;
+        case "today":
+            const today: Date = new Date();
+            processMessage(today, message);
+            break;
+        case "tomorrow":
+            const tomorrow: Date = new Date(new Date().getUTCFullYear(), new Date().getUTCMonth(), new Date().getUTCDate() + 1);
+            processMessage(tomorrow, message);
+            break;
+        case "day":
+            if (args.length == 0) {
+                message.reply("this command is incorrect. Please type `!help` to understand it.");
+                break;
+            }
+            const day: string = args[0].split("-")[0];
+            const month: string = args[0].split("-")[1];
+            const date: Date = new Date(new Date().getUTCFullYear(), +month - 1, +day);
+            processMessage(date, message);
+            break;
+        default:
+            message.reply("this command is unavailable. Please type `!help` to see available commands.");
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- src/ementas/Ementa.ts
+++ src/ementas/Ementa.ts
@@ -1,106 +1,96 @@
 import { EmentaType } from "./EmentaTypeEnum";
 
 class Ementa {
-  // Attributes
-  private date: Date;
-  private type: EmentaType;
+    // Attributes
+    private date: Date;
+    private type: EmentaType;
 
-  private sopa: string;
-  private carne: string;
-  private peixe: string;
-  private dieta: string;
-  private vegetariano: string;
+    private sopa: string;
+    private carne: string;
+    private peixe: string;
+    private dieta: string;
+    private vegetariano: string;
 
-  // Constructor
-  constructor(
-    date: Date,
-    type: EmentaType,
-    sopa: string,
-    carne: string,
-    peixe: string,
-    dieta: string,
-    vegetariano: string
-  ) {
-    this.date = date;
-    this.type = type;
+    // Constructor
+    constructor(date: Date, type: EmentaType, sopa: string, carne: string, peixe: string, dieta: string, vegetariano: string) {
+        this.date = date;
+        this.type = type;
 
-    this.sopa = sopa;
-    this.carne = carne;
-    this.peixe = peixe;
-    this.dieta = dieta;
-    this.vegetariano = vegetariano;
-  }
+        this.sopa = sopa;
+        this.carne = carne;
+        this.peixe = peixe;
+        this.dieta = dieta;
+        this.vegetariano = vegetariano;
+    }
 
-  // Getters & Setters
-  getDate(): Date {
-    return this.date;
-  }
+    // Getters & Setters
+    getDate(): Date {
+        return this.date;
+    }
 
-  setDate(date: Date) {
-    this.date = date;
-  }
+    setDate(date: Date) {
+        this.date = date;
+    }
 
-  getType(): EmentaType {
-    return this.type;
-  }
+    getType(): EmentaType {
+        return this.type;
+    }
 
-  setType(type: EmentaType) {
-    this.type = type;
-  }
+    setType(type: EmentaType) {
+        this.type = type;
+    }
 
-  getSopa(): string {
-    return this.sopa;
-  }
+    getSopa(): string {
+        return this.sopa;
+    }
 
-  setSopa(sopa: string) {
-    this.sopa = sopa;
-  }
+    setSopa(sopa: string) {
+        this.sopa = sopa;
+    }
 
-  getCarne(): string {
-    return this.carne;
-  }
+    getCarne(): string {
+        return this.carne;
+    }
 
-  setCarne(carne: string) {
-    this.carne = carne;
-  }
+    setCarne(carne: string) {
+        this.carne = carne;
+    }
 
-  getPeixe(): string {
-    return this.peixe;
-  }
+    getPeixe(): string {
+        return this.peixe;
+    }
 
-  setPeixe(peixe: string) {
-    this.peixe = peixe;
-  }
+    setPeixe(peixe: string) {
+        this.peixe = peixe;
+    }
 
-  getDieta(): string {
-    return this.dieta;
-  }
+    getDieta(): string {
+        return this.dieta;
+    }
 
-  setDieta(dieta: string) {
-    this.dieta = dieta;
-  }
+    setDieta(dieta: string) {
+        this.dieta = dieta;
+    }
 
-  getVegetariano(): string {
-    return this.vegetariano;
-  }
+    getVegetariano(): string {
+        return this.vegetariano;
+    }
 
-  setVegetariano(vegetariano: string) {
-    this.vegetariano = vegetariano;
-  }
+    setVegetariano(vegetariano: string) {
+        this.vegetariano = vegetariano;
+    }
 
-  // String to String
-  toString(): string {
-    let output: string = `**== ${
-      this.type == EmentaType.LUNCH ? "Almoço" : "Jantar"
-    } ${this.date.getUTCDate()}-${this.date.getUTCMonth() +
-      1}-${this.date.getUTCFullYear()} ==**`;
-    output += `\n*Sopa:* ${this.sopa}`;
-    output += `\n*Carne:* ${this.carne}`;
-    output += `\n*Peixe:* ${this.peixe}`;
-    output += `\n*Dieta:* ${this.dieta}`;
-    output += `\n*Vegetariano:* ${this.vegetariano}`;
-    return output;
-  }
+    // String to String
+    toString(): string {
+        let output: string = `**== ${this.type == EmentaType.LUNCH ? "Almoço" : "Jantar"} ${this.date.getUTCDate()}-${this.date.getUTCMonth() +
+            1}-${this.date.getUTCFullYear()} ==**`;
+        output += `\n*Sopa:* ${this.sopa}`;
+        output += `\n*Carne:* ${this.carne}`;
+        output += `\n*Peixe:* ${this.peixe}`;
+        output += `\n*Dieta:* ${this.dieta}`;
+        output += `\n*Vegetariano:* ${this.vegetariano}`;
+        return output;
+    }
 }
 
 export { Ementa, EmentaType };
